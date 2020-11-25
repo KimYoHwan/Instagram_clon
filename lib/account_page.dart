377da3c9ef44a3af7701745e603cb6960705c44d
Kim Yohwan <radline000@gmail.com>
2020-11-26 00:02:00 +0900

    Instagram_Clon Codeing Base End ! Time : 23:16 2020/11/26

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -12,6 +14,20 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  int _postCount =0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    FirebaseFirestore.instance.collection('post').where('email',isEqualTo: widget.user.email).getDocuments().then((snapshot){
+      setState(() {
+        _postCount = snapshot.documents.length;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +66,7 @@ class _AccountPageState extends State<AccountPage> {
                       height: 80.0,
                       child: CircleAvatar(
                         backgroundImage: NetworkImage(
-                            'https://www.dogdrip.net/dvs/d/20/07/26/100517a3e6386e8b15b6589ef115d182.png'),
+                            widget.user.photoURL),
                       )),
                   Container(
                     width: 80,
@@ -89,7 +105,7 @@ class _AccountPageState extends State<AccountPage> {
               )
             ],
           ),
-          Text('0\n게시물',
+          Text('$_postCount\n게시물',
               textAlign: TextAlign.center, style: TextStyle(fontSize: 18.0)),
           Text('0\n팔로워',
               textAlign: TextAlign.center, style: TextStyle(fontSize: 18.0)),
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -2,11 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clon/craete_page.dart';
+import 'package:instagram_clon/detail_post_page.dart';
 
 class SearchPage extends StatefulWidget {
   final User user;
 
   SearchPage(this.user);
+
   @override
   _SearchPageState createState() => _SearchPageState();
 }
@@ -17,38 +19,56 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>CreatePage(widget.user)));
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => CreatePage(widget.user)));
         },
         child: Icon(Icons.create),
         backgroundColor: Colors.blue,
       ),
-
     );
   }
 
   Widget _buildBody() {
     return StreamBuilder(
-      stream: Firestore.instance.collection('post').snapshots(), builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if(!snapshot.hasData){
-          return Center(child: CircularProgressIndicator(),);
+      stream: Firestore.instance.collection('post').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
-        var items = snapshot.data ?.documents ?? [];
-        return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 1.0,
-          mainAxisSpacing: 1.0,
-          crossAxisSpacing: 1.0,
-        ), itemBuilder: (context,index){
-          return _buildListItem(context,items[index]);
-        },
-          itemCount: items.length,);
-    },
-
+        var items = snapshot.data?.documents ?? [];
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 1.0,
+            mainAxisSpacing: 1.0,
+            crossAxisSpacing: 1.0,
+          ),
+          itemBuilder: (context, index) {
+            return _buildListItem(context, items[index]);
+          },
+          itemCount: items.length,
+        );
+      },
     );
   }
 
-  Widget _buildListItem(BuildContext context,document) {
-    return Image.network(document['photoUrl'],fit: BoxFit.cover,);
+  Widget _buildListItem(BuildContext context, document) {
+    return Hero(
+        tag: document['photoUrl'],
+        child: Material(
+            child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return DetailPostPage(document);
+                  }));
+                },
+                child: Image.network(
+                  document['photoUrl'],
+                  fit: BoxFit.cover,
+                ))));
   }
 }
